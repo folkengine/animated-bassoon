a9d0166ee60f59956b03b6888d77cee4800ace9c
Chris <ignatz@gmail.com>
2016-07-26 13:05:52 -0400

    first stab an property

--- src/roman_eqc.erl
+++ src/roman_eqc.erl
@@ -3,3 +3,37 @@
 -compile([export_all]).
 
 
+
+positive() ->
+	?LET(N, nat(), N + 1).
+
+roman(N) ->
+	?LET(R, N, decimal_to_roman(R)). 
+
+
+prop_roman_add() ->
+	?FORALL({X, Y}, {positive(), positive()},
+					equals(
+						roman:add_roman(roman(X), roman(y)),
+						roman(X + Y))).
+
+
+
+% eqc_gen:sample(eqc_gen:choose(1,5000)).
+
+
+decimal_to_roman(Num) ->
+    decimal_to_roman(Num, "",
+        [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+         "V", "IV", "I"]).
+decimal_to_roman(0, Accum, _, _) -> Accum;
+decimal_to_roman(Num, Accum, Decimals, Romans) ->
+    F_el = hd(Decimals),
+    if
+        F_el =< Num -> decimal_to_roman(Num - F_el,
+                                        Accum ++ hd(Romans),                    
+                                        Decimals, Romans);
+        true -> decimal_to_roman(Num, Accum, tl(Decimals), tl(Romans))
+    end.
+
